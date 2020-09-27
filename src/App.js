6c0190162b81d2c1f4af36e7e1446e259d64d2ea
Maxim <66437371+aaxsti@users.noreply.github.com>
2020-09-27 13:49:37 +0300

    2. grid

--- src/App.js
+++ src/App.js
@@ -1,13 +1,50 @@
 import React from 'react';
 import './App.css';
-import Technologies from "./Technologies";
-import Header from "./Header";
 
 const App = () => {
     return (
-        <div>
-            <Header/>
-            <Technologies/>
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img
+                    src='https://lh3.googleusercontent.com/proxy/0wCPQJ3qH0jXHRvDo7hfqNDoi5ZbuhU51JxoqLeOedfIwJrO8j3dP6yxfyzeZ9bKdzBdIxCwFfFq3koyHYEFfikaniDUyUR6ehSqwqo1Fz168ySwuhB6rgsMzcSTpcdePavH'/>
+            </header>
+            <nav className='nav'>
+                <div>
+                    Profile
+                </div>
+                <div>
+                    Messages
+                </div>
+                <div>
+                    News
+                </div>
+                <div>
+                    Music
+                </div>
+                <div>
+                    Settings
+                </div>
+            </nav>
+            <div className='content'>
+                <div>
+                    <img src='https://klike.net/uploads/posts/2019-09/1568880410_2.jpg'/>
+                </div>
+                <div>
+                    Avatar + descr
+                </div>
+                <div>
+                    My posts
+                    <div>
+                        New post
+                    </div>
+                    <div>
+                        post 1
+                    </div>
+                    <div>
+                        post 2
+                    </div>
+                </div>
+            </div>
         </div>
     )
 };
