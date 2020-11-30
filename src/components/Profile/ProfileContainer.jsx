4fa1cf0dae694c8a4d73ab59d4ec429a17ec9c55
Maxim <66437371+aaxsti@users.noreply.github.com>
2020-11-30 17:20:27 +0300

    58. using reselect library (reselect part 3)

--- src/components/Profile/ProfileContainer.jsx
+++ src/components/Profile/ProfileContainer.jsx
@@ -30,12 +30,14 @@ class ProfileContainer extends React.Component {
     }
 }
 
-let mapStateToProps = (state) => ({
-    profile: state.profilePage.profile,
-    status: state.profilePage.status,
-    authorizedUserId: state.auth.userId,
-    isAuth: state.auth.isAuth
-});
+let mapStateToProps = (state) => {
+    return ({
+        profile: state.profilePage.profile,
+        status: state.profilePage.status,
+        authorizedUserId: state.auth.userId,
+        isAuth: state.auth.isAuth
+    });
+}
 
 
 export default compose(
