e0eb680fe4a38932cc46280fcce65ffa5ec8869c
Maxim <66437371+aaxsti@users.noreply.github.com>
2021-06-23 13:45:39 +0300

    Updates

---
+++ precache-manifest.297e1695bae79a12a2f71e87e043f89d.js
@@ -0,0 +1,114 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "172992ef8e4fa0d0dd0e9fc2762ba693",
+    "url": "/social-react/index.html"
+  },
+  {
+    "revision": "7f38bd4279dd488cadc7",
+    "url": "/social-react/static/css/4.9b9c37bd.chunk.css"
+  },
+  {
+    "revision": "cee01b217e01a61736c3",
+    "url": "/social-react/static/css/main.bea62407.chunk.css"
+  },
+  {
+    "revision": "f77d028e789c6adc8950",
+    "url": "/social-react/static/js/0.c0a4185c.chunk.js"
+  },
+  {
+    "revision": "00789e124ee3bb3a0acb",
+    "url": "/social-react/static/js/1.607de8bd.chunk.js"
+  },
+  {
+    "revision": "d8568116daac490c3919",
+    "url": "/social-react/static/js/10.82c653bc.chunk.js"
+  },
+  {
+    "revision": "b410d53add621f2f7107",
+    "url": "/social-react/static/js/11.1b4523fe.chunk.js"
+  },
+  {
+    "revision": "21f9e9ad6e1aed95f414",
+    "url": "/social-react/static/js/12.35936341.chunk.js"
+  },
+  {
+    "revision": "7f38bd4279dd488cadc7",
+    "url": "/social-react/static/js/4.0ea104ec.chunk.js"
+  },
+  {
+    "revision": "6083d9c769f39b252a49c0c719a4b64c",
+    "url": "/social-react/static/js/4.0ea104ec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e8efbb1e45bb9ca1876b",
+    "url": "/social-react/static/js/5.6dc8bbcd.chunk.js"
+  },
+  {
+    "revision": "137cb7914bedc38d4d89",
+    "url": "/social-react/static/js/6.da272dd4.chunk.js"
+  },
+  {
+    "revision": "91035dbb2b8febe8fb4a",
+    "url": "/social-react/static/js/7.b98c85e3.chunk.js"
+  },
+  {
+    "revision": "2f9a9c04fbfe19ca581b",
+    "url": "/social-react/static/js/8.415cff0a.chunk.js"
+  },
+  {
+    "revision": "feb8e8d883249c136f2c",
+    "url": "/social-react/static/js/9.908a8952.chunk.js"
+  },
+  {
+    "revision": "cee01b217e01a61736c3",
+    "url": "/social-react/static/js/main.f1ab59da.chunk.js"
+  },
+  {
+    "revision": "d3a1176a6c9f6017ab8f",
+    "url": "/social-react/static/js/runtime-main.ddb1da8d.js"
+  },
+  {
+    "revision": "2b5f866f0b9d3fa7dce5023f60be18af",
+    "url": "/social-react/static/media/app-info-pic.2b5f866f.jpg"
+  },
+  {
+    "revision": "54f7e3dae678233b35dd624b558d65ed",
+    "url": "/social-react/static/media/belarus.54f7e3da.svg"
+  },
+  {
+    "revision": "19f49232b68156a2621ee826f21f7095",
+    "url": "/social-react/static/media/github.19f49232.svg"
+  },
+  {
+    "revision": "aabb690e6f6017285e31bc55053b7147",
+    "url": "/social-react/static/media/gmail.aabb690e.svg"
+  },
+  {
+    "revision": "4989ae0f419b1f8763bc60705609f682",
+    "url": "/social-react/static/media/linkedin.4989ae0f.svg"
+  },
+  {
+    "revision": "5d614f5e4627d5cbd4bee65c0cba054e",
+    "url": "/social-react/static/media/logo.5d614f5e.svg"
+  },
+  {
+    "revision": "3ba7224ee825ffd660ebc5e51e6dcef5",
+    "url": "/social-react/static/media/preloader.3ba7224e.svg"
+  },
+  {
+    "revision": "07a0b70120c6b64f978c88b1a514eced",
+    "url": "/social-react/static/media/sad.07a0b701.svg"
+  },
+  {
+    "revision": "71da0f1e24b16401cc0b16fbc9997356",
+    "url": "/social-react/static/media/telegram.71da0f1e.svg"
+  },
+  {
+    "revision": "57b075ed8d911cb5f5e5951923fba463",
+    "url": "/social-react/static/media/user.57b075ed.png"
+  },
+  {
+    "revision": "b5870904a978452cf2ffe155bf3196cb",
+    "url": "/social-react/static/media/vkontakte.b5870904.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/social-react/precache-manifest.7a149369a17a2ca725c96a253d4d0764.js"
+  "/social-react/precache-manifest.297e1695bae79a12a2f71e87e043f89d.js"
 );
 
 self.addEventListener('message', (event) => {
